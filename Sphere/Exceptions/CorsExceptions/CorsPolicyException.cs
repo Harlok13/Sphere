3089fdb82779bd0bdbbda72c43c5824345b2acc6
Harlok <holmik90@yandex.ru>
2023-11-16 21:23:44 +0400

    feat: custom exseption for undefined current CORS policy

---
+++ Sphere/Exceptions/CorsExceptions/CorsPolicyException.cs
@@ -0,0 +1,9 @@
+namespace Sphere.Exceptions.CorsExceptions;
+
+public class CorsPolicyException : ApplicationException
+{
+    public CorsPolicyException(string message) : base(message)
+    {
+        
+    }
+}
