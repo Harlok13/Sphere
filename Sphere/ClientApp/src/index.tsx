689ad9cc746a92910a741e02daebb16634e4b4e0
Harlok <holmik90@yandex.ru>
2023-11-16 12:00:29 +0400

    Initial commit

---
+++ Sphere/ClientApp/src/index.tsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href')!;
+const rootElement = document.getElementById('root')!;
+const root = createRoot(rootElement)!;
+
+root.render(
+  <BrowserRouter basename={baseUrl}>
+    <App />
+  </BrowserRouter>);
+
+serviceWorkerRegistration.unregister();
+
