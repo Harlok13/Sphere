8cbcac703f8ff064f338914d9b925560deb3e99b
Harlok <holmik90@yandex.ru>
2023-11-17 23:43:41 +0400

    fix: add a model for user statistic

---
+++ Sphere/DAL/Models/UserStatisticModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Sphere.DAL.Models;
+
+public class UserStatisticModel : BaseModel  
+{
+    public int Id { get; init; }
+
+    public int Matches { get; set; }
+
+    public int Loses { get; set; }
+
+    public int Wins { get; set; }
+
+    public int Draws { get; set; }
+
+    public int Exp { get; set; }
+
+    public int Money { get; set; }
+
+    public int Likes { get; set; }
+
+    public int Level { get; set; }
+
+    public int Has21 { get; set; }
+
+    public UserStatisticModel(int id) => Id = id;
+}
