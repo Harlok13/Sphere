81ef88e7d73189c7b4406c107f108d6b0830ec26
Harlok <holmik90@yandex.ru>
2023-11-17 23:31:15 +0400

    feat: add role constants for application user

---
+++ Sphere/DTO/Auth/RoleConstants.cs
@@ -0,0 +1,10 @@
+namespace Sphere.DTO.Auth;
+
+public class RoleConstants
+{
+    public const string Member = "Member";
+    
+    public const string Moderator = "Moderator";
+    
+    public const string Administrator = "Administrator";
+}
