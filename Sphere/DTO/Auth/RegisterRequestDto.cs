b2ea06f472beb3f23af41e590ed9d49f6f7697c5
Harlok <holmik90@yandex.ru>
2023-11-17 23:32:15 +0400

    feat: add dto model for user registration

--- Sphere/DTO/Auth/RegisterRequestDto.cs
+++ Sphere/DTO/Auth/RegisterRequestDto.cs
@@ -6,20 +6,20 @@ public class RegisterRequestDto
 {
     [Required] 
     [Display(Name = "Email")] 
-    public string Email { get; set; } = null!;
-    
+    public string Email { get; set; }
+
     [Required]
     [Display(Name = "Username")]
-    public string UserName { get; set; } = null!;
+    public string UserName { get; set; }
 
     [Required]
     [DataType(DataType.Password)]
     [Display(Name = "Password")]
-    public string Password { get; set; } = null!;
+    public string Password { get; set; }
 
     [Required]
     [Compare("Password", ErrorMessage = "Passwords doesn't match")]
     [DataType(DataType.Password)]
     [Display(Name = "Confirm password")]
-    public string PasswordConfirm { get; set; } = null!;
+    public string PasswordConfirm { get; set; }
 }
