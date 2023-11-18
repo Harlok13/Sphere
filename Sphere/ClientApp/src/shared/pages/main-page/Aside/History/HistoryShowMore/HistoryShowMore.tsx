294b18b8f4e5a994fe0a19af1f8c4014e50ea99d
Harlok <holmik90@yandex.ru>
2023-11-18 23:39:04 +0400

    feat: add HistoryShowMore component

---
+++ Sphere/ClientApp/src/shared/pages/main-page/Aside/History/HistoryShowMore/HistoryShowMore.tsx
@@ -0,0 +1,12 @@
+// @ts-ignore
+import style from "./style.module.css";
+
+const HistoryShowMore = () => {
+    return (
+        <button className={style.showMore}>
+            Show more
+        </button>
+    )
+}
+
+export {HistoryShowMore};
