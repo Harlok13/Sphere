95b3e51b5b4f88bd86391b82c17760dc17f9b606
Harlok <holmik90@yandex.ru>
2023-11-19 23:45:32 +0400

    feat: add UserInfoBody component

---
+++ Sphere/ClientApp/src/shared/pages/main-page/Aside/UserInfo/UserInfoBody/UserInfoBody.tsx
@@ -0,0 +1,11 @@
+// @ts-ignore
+import style from "./UserInfoBody.module.css";
+import {PropsWithChildren} from "react";
+
+export const UserInfoBody = ({children}: PropsWithChildren<{}>) => {
+    return (
+        <div className={style.body}>
+            {children}
+        </div>
+    )
+}
