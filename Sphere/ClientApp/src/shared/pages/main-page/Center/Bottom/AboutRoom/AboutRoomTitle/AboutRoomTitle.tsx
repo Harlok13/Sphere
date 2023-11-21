2848dd5342f981fea8b2e3e1f0e30cc1f2763116
Harlok <holmik90@yandex.ru>
2023-11-21 23:49:49 +0400

    feat: add AboutRoomTitle component

---
+++ Sphere/ClientApp/src/shared/pages/main-page/Center/Bottom/AboutRoom/AboutRoomTitle/AboutRoomTitle.tsx
@@ -0,0 +1,9 @@
+import style from "./AboutRoomTitle.module.css";
+
+const AboutRoomTitle = () => {
+    return (
+        <div className={style.title}>Room info</div>
+    )
+}
+
+export {AboutRoomTitle};
