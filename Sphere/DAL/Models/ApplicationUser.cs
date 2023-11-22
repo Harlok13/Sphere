e28d8bf3f8b95ead11112abd7ccca6bb208eae67
Harlok <holmik90@yandex.ru>
2023-11-22 23:41:43 +0400

    feat: add Avatar field

--- Sphere/DAL/Models/ApplicationUser.cs
+++ Sphere/DAL/Models/ApplicationUser.cs
@@ -4,6 +4,8 @@ namespace Sphere.DAL.Models;
 
 public class ApplicationUser : IdentityUser<int>
 {
+    public string Avatar { get; set; } = "/img/avatars/default_avatar.png";  // TODO: relocate to settings and add configurations
+    
     public string? RefreshToken { get; set; }
     
     public DateTime RefreshTokenExpiryTime { get; set; }
