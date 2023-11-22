6abcbdf52919003b8c5674af2c356ad8aa7c7752
Harlok <holmik90@yandex.ru>
2023-11-22 23:51:19 +0400

    feat: add GlobalRightSide component

---
+++ Sphere/ClientApp/src/components/layout/GlobalRightSide/GlobalRightSide.tsx
@@ -0,0 +1,9 @@
+import {RightSide} from "../../../shared/pages/main-page/RightSide/RightSide";
+
+export const GlobalRightSide = () => {
+    return (
+        <RightSide>
+
+        </RightSide>
+    )
+}
