5dabbf21c61f54ea70f827f7f0ec6a96ac57fbd4
Harlok <holmik90@yandex.ru>
2023-12-16 23:16:54 +0400

    feat: add moneyReducer and notificationsReducer. add RootState. rename userInfo to playerInfo

--- Web.ClientApp/src/BL/store.ts
+++ Web.ClientApp/src/BL/store.ts
@@ -1,14 +1,20 @@
 import {configureStore} from "@reduxjs/toolkit";
-import game21Reducer from "./slices/game21.slice";
-import userInfoReducer from "./slices/user-info.slice";
-import lobbyReducer from "./slices/lobby.slice";
-import playerReducer from "./slices/player.slice";
+import game21Reducer from "./slices/game21/game21.slice";
+import playerInfoReducer from "./slices/player-info/player-info.slice";
+import lobbyReducer from "./slices/lobby/lobby.slice";
+import playerReducer from "./slices/player/player.slice";
+import moneyReducer from "./slices/money/money.slice";
+import notificationsReducer from "./slices/notifications/notifications";
 
-export default configureStore({
+export const store = configureStore({
     reducer: {
         game21: game21Reducer,
-        userInfo: userInfoReducer,
+        playerInfo: playerInfoReducer,
         lobby: lobbyReducer,
         player: playerReducer,
+        money: moneyReducer,
+        notifications: notificationsReducer
     }
 });
+
+export type RootState = ReturnType<typeof store.getState>
