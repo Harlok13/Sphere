b4163669327e8968d14bafd1b926eaa6046c54e0
Harlok <holmik90@yandex.ru>
2023-12-16 23:26:22 +0400

    fix: remove ApplicationUser and rename PlayerStatistic to PlayerInfo

--- App.Contracts/Responses/InitDataResponse.cs
+++ App.Contracts/Responses/InitDataResponse.cs
@@ -4,8 +4,8 @@ using App.Domain.Identity.Entities;
 namespace App.Contracts.Responses;
 
 public sealed record InitDataResponse(
-    ApplicationUser UserResponse,
     PlayerResponse? PlayerResponse,
-    PlayerStatisticResponse PlayerStatisticResponse,
-    IEnumerable<PlayerHistoryResponse>? PlayerHistoryResponse,
-    IEnumerable<RoomResponse>? RoomResponse);
+    PlayerInfoResponse PlayerInfoResponse,
+    IEnumerable<PlayerHistoryResponse>? PlayerHistoriesResponse,
+    IEnumerable<RoomResponse>? RoomsResponse);
+    
