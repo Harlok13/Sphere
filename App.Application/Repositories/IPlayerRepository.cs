b833b4b308a410ce18387d3f14299a2983b9da4c
Harlok <holmik90@yandex.ru>
2023-12-16 23:24:29 +0400

    feat: add RemovePlayerAsync

--- App.Application/Repositories/IPlayerRepository.cs
+++ App.Application/Repositories/IPlayerRepository.cs
@@ -7,4 +7,5 @@ public interface IPlayerRepository
 {
     Task<Player?> GetPlayerByIdAsync(Guid id, CancellationToken cT);
     Task CreatePlayerAsync(Player player, CancellationToken cT);
+    Task RemovePlayerAsync(Guid playerId, CancellationToken cT);
 }
