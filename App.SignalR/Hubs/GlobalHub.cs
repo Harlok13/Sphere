7a6862b0185206585ab1378f7536b41886500847
Harlok <holmik90@yandex.ru>
2023-12-27 23:17:30 +0400

    ref: ref methods

--- App.SignalR/Hubs/GlobalHub.cs
+++ App.SignalR/Hubs/GlobalHub.cs
@@ -50,7 +50,6 @@ public class GlobalHub : Hub<IGlobalHub>
     
     public async ValueTask<bool> CreateRoom(CreateRoomRequest request)
     {
-        _logger.LogInformation($"Invoke method \"CreateRoom\" by user {request.PlayerId}. Create room {request.RoomRequest.RoomName}");
         var command = new CreateRoomCommand(
             Request: request,
             ConnectionId: Context.ConnectionId);
@@ -59,7 +58,6 @@ public class GlobalHub : Hub<IGlobalHub>
     
     public async ValueTask<bool> JoinToRoom(JoinToRoomRequest request)
     {
-        _logger.LogInformation($"Invoke method \"JoinToRoom\" by user {request.PlayerId} in room {request.RoomId}.");
         var command = new JoinToRoomCommand(
             Request: request,
             ConnectionId: Context.ConnectionId);
@@ -68,41 +66,23 @@ public class GlobalHub : Hub<IGlobalHub>
 
     public async ValueTask<bool> RemoveFromRoom(RemoveFromRoomRequest request)
     {
-        _logger.LogInformation($"Invoke method \"RemoveFromRoom\" by user {request.PlayerId} in room {request.RoomId}.");
         var command = new RemoveFromRoomCommand(
             Request: request,
             ConnectionId: Context.ConnectionId);
         return await _mediator.Send(command);
     }
 
-    public async ValueTask<bool> SelectStartGameMoney(SelectStartGameMoneyRequest request)
-    {
-        _logger.LogInformation($"Invoke method \"{nameof(SelectStartGameMoney)}\" by user {request.PlayerId}.");
-        var command = new SelectStartGameMoneyCommand(
-            Request: request,
-            ConnectionId: Context.ConnectionId);
-        return await _mediator.Send(command);
-    }
+    public async ValueTask<bool> SelectStartGameMoney(SelectStartGameMoneyRequest request) =>
+        await _mediator.Send(new SelectStartGameMoneyCommand(request));
 
-    public async ValueTask<bool> ToggleReadiness(ToggleReadinessRequest request)
-    {
-        _logger.LogInformation($"Invoke method \"ToggleReadiness\" by user {request.PlayerId} in room {request.RoomId}.");
-        var command = new ToggleReadinessCommand(
-            Request: request);
-        return await _mediator.Send(command);
-    }
+    public async ValueTask<bool> ToggleReadiness(ToggleReadinessRequest request) =>
+        await _mediator.Send(new ToggleReadinessCommand(request));
 
-    public async ValueTask<bool> StartGame(StartGameRequest request)
-    {
-        _logger.LogInformation($"Invoke method \"StartGame\" by user {request.PlayerId} in room {request.RoomId}");
-        var command = new StartGameCommand(Request: request);
-        return await _mediator.Send(command);
-    }
+    public async ValueTask<bool> StartGame(StartGameRequest request) =>
+        await _mediator.Send(new StartGameCommand(request));
 
     public async ValueTask<bool> StartTimer(StartTimerRequest request)
     {
-        _logger.LogInformation($"Invoke method \"StartTimer\" by user {request.PlayerId} in room {request.RoomId}");
-        
         var cts = new CancellationTokenSource();
         Context.Items.TryAdd(Context.ConnectionId, cts);
 
@@ -110,17 +90,16 @@ public class GlobalHub : Hub<IGlobalHub>
             Request: request,
             ConnectionId: Context.ConnectionId, 
             Cts: cts);
+        
         return await _mediator.Send(command);
     }
 
     public async ValueTask<bool> StopTimer(StopTimerRequest request)
     {
-        _logger.LogInformation($"Invoke method \"StopTimer\" by user {request.PlayerId} in room {request.RoomId}.");
-        
         var result = Context.Items.TryGetValue(Context.ConnectionId, out var cts);
         Context.Items.Remove(Context.ConnectionId);
         
-        _logger.LogInformation($"Result of getting cts is {result}. Cts is null? {cts is null}.");
+        _logger.LogInformation("Result of getting cts is {Result}.", result);
         
         var command = new StopTimerCommand(
             Request: request,
