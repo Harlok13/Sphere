f2a3d880d64c07d99c119264fc97436a99d2a36b
Harlok <holmik90@yandex.ru>
2023-12-28 23:48:17 +0400

    feat: add fields - Players, Bank, Id

--- App.Contracts/Data/RoomDto.cs
+++ App.Contracts/Data/RoomDto.cs
@@ -1,11 +1,17 @@
+using App.Contracts.Responses;
+using App.Domain.Enums;
+
 namespace App.Contracts.Data;
 
 public sealed record RoomDto(
+    Guid Id,
     string RoomName,
     int RoomSize,
     int StartBid,
     int MinBid,
     int MaxBid,
     string AvatarUrl,
-    string Status,
-    int PlayersInRoom);
+    RoomStatus Status,
+    int PlayersInRoom,
+    IEnumerable<PlayerDto> Players, 
+    int Bank);
