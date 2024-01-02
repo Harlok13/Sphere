64349e272a70104198ee48b0ef0470bdc6c6f36d
Harlok <holmik90@yandex.ru>
2024-01-02 23:40:34 +0400

    chore: remove extra code

--- App.Domain/Entities/PlayerInfoEntity/PlayerInfoEvents.cs
+++
@@ -1,8 +0,0 @@
-using App.Domain.Primitives;
-
-namespace App.Domain.Entities.PlayerInfoEntity;
-
-public sealed partial class PlayerInfo
-{
-    public event EventHandlerAsync<PlayerInfo>? NotifyMoney;
-}
