34904fcf53fd499bcb5d3b6b63a7e6be47563a9b
Harlok <holmik90@yandex.ru>
2024-01-07 23:49:03 +0400

    ref: rename namespace PlayerActionHandlers -> ParticipantActionHandlers

--- App.Application/Handlers/RoomHandlers/ParticipantActionHandlers/TransferLeadershipHandler.cs
+++ App.Application/Handlers/RoomHandlers/ParticipantActionHandlers/TransferLeadershipHandler.cs
@@ -8,7 +8,7 @@ using App.SignalR.Events;
 using Mediator;
 using Microsoft.Extensions.Logging;
 
-namespace App.Application.Handlers.RoomHandlers.PlayerActionHandlers;
+namespace App.Application.Handlers.RoomHandlers.ParticipantActionHandlers;
 
 public class TransferLeadershipHandler : ICommandHandler<TransferLeadershipCommand, bool>
 {
