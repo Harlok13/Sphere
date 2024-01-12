76323884bad608e400efd9eb3eef54177005baa5
Harlok <holmik90@yandex.ru>
2024-01-12 14:54:59 +0400

    chore: remove extra code

--- App.Application/Services/Interfaces/IGame21Service.cs
+++
@@ -1,9 +0,0 @@
-using App.Domain.Entities;
-using App.Domain.Entities.RoomEntity;
-
-namespace App.Application.Services.Interfaces;
-
-public interface IGame21Service
-{
-    void StartGame(Room room);
-}
