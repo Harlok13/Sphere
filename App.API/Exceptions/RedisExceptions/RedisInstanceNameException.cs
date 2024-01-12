b259df7f3020affab037e4e17fcf3f9f3f511037
Harlok <holmik90@yandex.ru>
2024-01-13 00:42:50 +0400

    feat: add RedisInstanceNameException

---
+++ App.API/Exceptions/RedisExceptions/RedisInstanceNameException.cs
@@ -0,0 +1,9 @@
+namespace Sphere.Exceptions.RedisExceptions;
+
+public class RedisInstanceNameException : ApplicationException
+{
+    public RedisInstanceNameException(string message) : base(message)
+    {
+        
+    }
+}
