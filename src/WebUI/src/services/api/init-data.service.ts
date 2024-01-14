049d055f54b720f13813c3b20bf57bf3551abf4f
Harlok <holmik90@yandex.ru>
2024-01-14 19:00:03 +0400

    chore: put the url into a variable

--- src/WebUI/src/services/api/init-data.service.ts
+++ src/WebUI/src/services/api/init-data.service.ts
@@ -3,11 +3,14 @@ import axios from "axios";
 import UserService from "services/user/user.service";
 
 class InitDataService {  // TODO: finish
-    private readonly URL = "http://localhost:5083/api/init_data";  // TODO: relocate to settings
+    // private readonly URL = "http://localhost:5083/api/init_data";  
+    private readonly URL = "https://localhost:7170/api/init_data";  // TODO: relocate to settings
 
     async getInitData(): Promise<IInitDataResponse>{
-        // const {data} = await API.get(`api/init_data/${UserService.getUser().userId}`)
-        const {data} = await axios.get<IInitDataResponse>(`${this.URL}/${UserService.getUser().userId}`)
+        const {data} = await axios.get<IInitDataResponse>(`${this.URL}/${UserService.getUser().userId}`,{
+            headers: {"Authorization": `Bearer ${UserService.getUser().token}`},
+            withCredentials: true
+        })
 
         return data;
     }
